bfa7f317bc2d375a68fd6bb0b6138776bf5fbe27
Helmuth Hernandez <helmuth.hernandez.alcazar@alumnos.upm.es>
2021-11-02 18:13:19 +0100

    AddFirst and IndexOf tests

---
+++ src/test/java/com/practica/cajanegra/AddFirstTest.java
@@ -0,0 +1,24 @@
+package com.practica.cajanegra;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.cajanegra.*;
+
+public class AddFirstTest {
+	private SingleLinkedListImpl<String> lista;
+	
+	@BeforeEach
+	public void crearLista() {
+		this.lista = new SingleLinkedListImpl<String>("A", "S", "C", "I");
+	}
+	
+	@ParameterizedTest(name="Añadir {0} al principio")
+	@ValueSource(strings= {"A", "B", "M", "Y", "Z", "@", "["} )
+	public void addFirst(String s) {
+		this.lista.addFirst(s);
+		assertEquals("["+ s + " ,A, S, C, I]", this.lista.toString());	
+	}
+	
+}
---
+++ src/test/java/com/practica/cajanegra/IndexOfTest.java
@@ -0,0 +1,52 @@
+package com.practica.cajanegra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.cajanegra.SingleLinkedListImpl;
+
+public class IndexOfTest {
+	
+	private SingleLinkedListImpl<String> lista;
+	
+	/*@BeforeEach
+	public void crearLista() {
+		this.lista = new SingleLinkedListImpl<String>("A", "S", "C", "I", "B", "Y", "Z", "B", "B", "M");
+	}*/
+	
+	@ParameterizedTest(name="Posición primera aparición de {0}")
+	@ValueSource(strings= {"A", "B", "M", "Y", "Z"})
+	public void IndexOfSEnLista(String s) {
+		
+		this.lista = new SingleLinkedListImpl<String>("A", "S", "C", "I", "B", "Y", "Z", "B", "B", "M");
+		Map<String, Integer> resultadosEsperados = new HashMap<String, Integer>();
+		resultadosEsperados.put("A", 1);
+		resultadosEsperados.put("B", 5);
+		resultadosEsperados.put("M", 10);
+		resultadosEsperados.put("Y", 6);
+		resultadosEsperados.put("Z", 7);
+		
+		assertEquals(resultadosEsperados.get(s), lista.indexOf(s));
+	}
+	
+	@ParameterizedTest(name="Posición de {0} en lista que no aparece")
+	@ValueSource(strings= {"A", "B", "M", "Y", "Z", "@", "["})
+	public void IndexOfSNoEnLista(String s) {
+		
+		this.lista = new SingleLinkedListImpl<String>("J", "U", "E", "V", "E", "S");
+		
+		assertThrows(NoSuchElementException.class, () ->{
+			lista.indexOf(s);
+		});
+	}
+	
+}
